f9010ed8c9aef2f00fdfbc8e609dff04630122ba
chenqg <1014115430@qq.com>
2021-07-29 23:15:13 +0800

    nothing

--- demo-thread/src/main.cpp
+++ demo-thread/src/main.cpp
@@ -6,62 +6,21 @@
 #include <vector>
 #include <chrono>
 #include <list>
-
-
+#include <memory>
+#include <unordered_map>
+#include <iostream>
 using namespace std;
 
-std::mutex mtx;
-std::condition_variable cv_p,cv_c;
-std::queue<size_t> m_queuw;
-size_t m_data = 1;
-
-void producer(const int& num)
-{
-	while (true)
-	{
-		std::unique_lock<std::mutex> lck(mtx);
-		cv_p.wait(lck, [](){return m_queuw.size() <= 100; });
-		m_queuw.push(m_data++);
-		cout << "producer: " << num << " queue size: " << m_queuw.size() << endl;
-		//cout << "producer: " << num << " data: " << m_data << endl;
-		cv_c.notify_one();
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
-	}
-}
-
-void comsumer(int a)
-{
-	while (true)
-	{
-		std::unique_lock<std::mutex> lck(mtx);
-		cv_c.wait(lck, []() {return !m_queuw.empty(); });
-		a = m_queuw.front();
-		m_queuw.pop();
-		//cout << "comsumer: " << a << " queue size: " << m_queuw.size() << endl;
-		//cout << "comsumer: " << a << " data: " << m_data << endl;
-		cv_p.notify_one();
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
-	}
-};
 
 int main()
 {	
-	std::thread pros[10],consumers[4];
-	vector <int> v1;
-	v1.emplace_back(2);
-	list <int> lis1;
-	lis1.emplace_back(2);
-	lis1.push_back(0);
-
-	for (int i = 0; i < 10; i++)
+	vector<float> a (1000, 0.1f);	
+	unique_ptr<float[]> tmp(new float[1000]);
+	for (int i = 0; i < 1000; i++)
 	{
-		pros[i]=std::thread(&producer,i);
-		pros[i].join();
+		tmp[i] = i * 0.001f;
 	}
-	for (int i = 0; i < 4; i++)
-	{
-		consumers[i] = std::thread(&comsumer, i);		
-		consumers[i].join();
-	}
+	cout << "unique_ptr<float[]>: " << sizeof(tmp) << endl;
+	system("pause");
 	return 0;
 }
